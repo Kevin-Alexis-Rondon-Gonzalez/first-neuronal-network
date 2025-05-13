97ae6e611b0a29072afdd7581b8e751d263a3c76
Kevin Alexis Rondon Gonzalez <kevinalexisrondongonzalez@gmail.com>
2025-05-13 17:53:10 -0400

    adding layers

--- first_neuronal_network.py
+++ first_neuronal_network.py
@@ -5,8 +5,14 @@ import matplotlib.pyplot as plt
 celsius = np.array([-40,-10,0,8,15,22,38],dtype = float)
 fahrenheit = np.array([-40,14,32,46,59,72,100],dtype = float)
 
+"""
 capa = tf.keras.layers.Dense(units=1, input_shape=[1])
 modelo = tf.keras.Sequential([capa])
+"""
+oculta1 = tf.keras.layers.Dense(units=3, input_shape=[1])
+oculta2 = tf.keras.layers.Dense(units=3)
+salida = tf.keras.layers.Dense(units=1)
+modelo = tf.keras.Sequential([oculta1,oculta2,salida])
 
 modelo.compile(
     optimizer = tf.keras.optimizers.Adam(0.1),
@@ -26,3 +32,8 @@ plt.show()
 print("hagamos prediccion")
 resultado = modelo.predict(np.array([100.0]))
 print("resuultado es " + str(resultado) +"fahrenhait")
+
+"""
+print("variables inernas del modelo")
+print(capa.get_weights()) #know the weights assigned to the neuronal
+"""
